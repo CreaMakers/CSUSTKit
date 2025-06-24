0ff44da853f1eaeb83f7dedc0af1d3ef1ef017a2
zHElEARN <personal@zhelearn.com>
2025-06-24 22:36:53 +0800

    feat: initialize project

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "csust-api-swift",
+    targets: [
+        .executableTarget(name: "csust-api-swift")
+    ]
+)
