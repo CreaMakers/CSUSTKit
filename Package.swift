38299b0d15e23dd7ce6093d9d548feef6beb2c09
zHElEARN <personal@zhelearn.com>
2025-06-25 21:54:18 +0800

    feat: update package dependencies

--- Package.swift
+++ Package.swift
@@ -4,15 +4,20 @@ import PackageDescription
 
 let package = Package(
     name: "csust-api-swift",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.0"))
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.10.0")),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.7.6"),
     ],
     targets: [
         .executableTarget(
             name: "csust-api-swift",
             dependencies: [
-                .product(name: "Alamofire", package: "Alamofire")
-            ]
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "SwiftSoup", package: "SwiftSoup"),
+            ],
         )
     ]
 )
