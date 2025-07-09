8878287fc7990f3d6f0e8dfd58487a289e517d7c
zHElEARN <personal@zhelearn.com>
2025-07-09 16:03:16 +0800

    feat: add initializer for Building struct

--- Sources/CSUSTKit/CampusCard/Models/Building.swift
+++ Sources/CSUSTKit/CampusCard/Models/Building.swift
@@ -1,4 +1,10 @@
 public struct Building: Sendable {
+    public init(name: String, id: String, campus: Campus) {
+        self.name = name
+        self.id = id
+        self.campus = campus
+    }
+
     public let name: String
     public let id: String
     public let campus: Campus
