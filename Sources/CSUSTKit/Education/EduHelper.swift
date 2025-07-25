1da3393946c14e58d21172c479ba64c06efa60a2
zHElEARN <personal@zhelearn.com>
2025-07-25 10:29:11 +0800

    feat: enhance EduHelper initialization with session management and add EduRequestInterceptor for request retries

--- Sources/CSUSTKit/Education/EduHelper.swift
+++ Sources/CSUSTKit/Education/EduHelper.swift
@@ -11,13 +11,25 @@ public class EduHelper {
     public let profileService: ProfileService
     public let semesterService: SemesterService
 
-    public init(session: Session = Session()) {
-        self.session = session
+    public init(session: Session? = nil) {
+        let interceptor = EduRequestInterceptor(maxRetryCount: 5)
+        if let baseSession = session {
+            let cookies = baseSession.sessionConfiguration.httpCookieStorage?.cookies ?? []
+            let configuration = URLSessionConfiguration.default
+            configuration.httpCookieStorage = HTTPCookieStorage.shared
+            for cookie in cookies {
+                configuration.httpCookieStorage?.setCookie(cookie)
+            }
+            self.session = Session(
+                configuration: configuration, interceptor: interceptor)
+        } else {
+            self.session = Session(interceptor: interceptor)
+        }
 
-        authService = AuthService(session: session)
-        courseService = CourseService(session: session)
-        examService = ExamService(session: session)
-        profileService = ProfileService(session: session)
-        semesterService = SemesterService(session: session)
+        authService = AuthService(session: self.session)
+        courseService = CourseService(session: self.session)
+        examService = ExamService(session: self.session)
+        profileService = ProfileService(session: self.session)
+        semesterService = SemesterService(session: self.session)
     }
 }
