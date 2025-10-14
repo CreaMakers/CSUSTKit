3ddc3715bdaa5c11dcf4b16fece76154fadd6a71
zHElEARN <personal@zhelearn.com>
2025-10-14 15:47:30 +0800

    fix(教务系统): 修复个人信息获取错误的问题

--- Sources/CSUSTKit/Education/Services/ProfileService.swift
+++ Sources/CSUSTKit/Education/Services/ProfileService.swift
@@ -3,6 +3,18 @@ import SwiftSoup
 extension EduHelper {
     /// 个人档案服务
     public class ProfileService: BaseService {
+        private func parseTableCell(_ rows: Elements, _ rowIndex: Int, _ colIndex: Int) throws -> String {
+            guard rowIndex < rows.count else {
+                throw EduHelperError.profileRetrievalFailed("行索引越界")
+            }
+            let row = rows[rowIndex]
+            let cols = try row.select("td")
+            guard colIndex < cols.count else {
+                throw EduHelperError.profileRetrievalFailed("列索引越界")
+            }
+            return try cols[colIndex].text().trim()
+        }
+
         /// 获取学生档案信息
         /// - Throws: `EduHelperError`
         /// - Returns: 学生档案信息
@@ -13,17 +25,6 @@ extension EduHelper {
                 throw EduHelperError.profileRetrievalFailed("未找到个人信息表")
             }
             let rows = try table.select("tr")
-            func parseTableCell(_ rows: Elements, _ rowIndex: Int, _ colIndex: Int) throws -> String {
-                guard rowIndex < rows.count else {
-                    throw EduHelperError.profileRetrievalFailed("行索引越界")
-                }
-                let row = rows[rowIndex]
-                let cols = try row.select("td")
-                guard colIndex < cols.count else {
-                    throw EduHelperError.profileRetrievalFailed("列索引越界")
-                }
-                return try cols[colIndex].text().trim()
-            }
             return Profile(
                 department: try parseTableCell(rows, 2, 0).components(separatedBy: "：")[1],
                 major: try parseTableCell(rows, 2, 1).components(separatedBy: "：")[1],
@@ -34,11 +35,11 @@ extension EduHelper {
                 gender: try parseTableCell(rows, 3, 3),
                 namePinyin: try parseTableCell(rows, 3, 5),
                 birthDate: try parseTableCell(rows, 4, 1),
-                ethnicity: try parseTableCell(rows, 4, 5),
-                studyLevel: try parseTableCell(rows, 7, 3),
-                homeAddress: try parseTableCell(rows, 8, 3),
-                homePhone: try parseTableCell(rows, 9, 1),
-                personalPhone: try parseTableCell(rows, 10, 3),
+                ethnicity: try parseTableCell(rows, 7, 3),
+                studyLevel: try parseTableCell(rows, 8, 3),
+                homeAddress: try parseTableCell(rows, 9, 1),
+                homePhone: try parseTableCell(rows, 10, 3),
+                personalPhone: try parseTableCell(rows, 4, 5),
                 enrollmentDate: try parseTableCell(rows, 46, 1),
                 entranceExamID: try parseTableCell(rows, 47, 1),
                 idCardNumber: try parseTableCell(rows, 47, 3)
